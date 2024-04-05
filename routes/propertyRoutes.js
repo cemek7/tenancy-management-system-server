e0c4884536bc480898b88b6f0f588ec67eabab07
cemek7 <106226729+cemek7@users.noreply.github.com>
2024-04-05 16:12:23 +0100

    Update propertyRoutes.js

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -5,8 +5,12 @@ const router = express.Router();
 const propertyController = require('../controllers/propertyController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.get('/', authMiddleware.authenticate, propertyController.getProperties);
-router.post('/', authMiddleware.authenticate, propertyController.addProperty);
+
+router.post('/', propertyController.createProperty);
+router.get('/', propertyController.getAllProperties);
+router.get('/:id', propertyController.getPropertyById);
+router.put('/:id', propertyController.updateProperty);
+router.delete('/:id', propertyController.deleteProperty);
 router.get('/:id', authMiddleware.authenticate, propertyController.getPropertyDetails);
 
 // Other property-related routes (e.g., create property, update property, delete property) can be added here
