73450257af0182d43a234f1da7a9bb6af1288ee6
cemek7 <106226729+cemek7@users.noreply.github.com>
2024-04-05 16:20:43 +0100

    Create expenseRoutes.js

---
+++ routes/expenseRoutes.js
@@ -0,0 +1,13 @@
+// routes/expenseRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const expenseController = require('../controllers/expenseController');
+
+router.post('/', expenseController.createExpense);
+router.get('/', expenseController.getAllExpenses);
+router.get('/:id', expenseController.getExpenseById);
+router.put('/:id', expenseController.updateExpense);
+router.delete('/:id', expenseController.deleteExpense);
+
+module.exports = router;
